3e2bb33ca5b1de62c8c06025f24f7ef4e61faaa5
hcssmith <66956565+hcssmith@users.noreply.github.com>
2023-08-19 22:02:48 +0100

    Fix config to be function / add treesitter-context

--- lua/coreconfig/init.lua
+++ lua/coreconfig/init.lua
@@ -11,6 +11,8 @@ require("coreconfig.plugins.lsp")
 
 require("coreconfig.plugins.telescope")
 
+require("coreconfig.plugins.ufo")
+
 if vim.g.neovide then
   require("coreconfig.gui")
 end
--- lua/coreconfig/packer.lua
+++ lua/coreconfig/packer.lua
@@ -1,109 +1,116 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
+	use 'wbthomason/packer.nvim'
 
-  use {
-    'lewis6991/gitsigns.nvim',
-    config = require('gitsigns').setup();
-  }
+	use {'kevinhwang91/nvim-ufo', requires = 'kevinhwang91/promise-async'}
 
-  use 'vim-crystal/vim-crystal'
+	 use {
+	    'lewis6991/gitsigns.nvim',
+	    config = function () require('gitsigns').setup() end
+	  }
 
-  use {
-    'folke/tokyonight.nvim',
-    config = require('tokyonight').setup({
-      style = 'storm'
-    })
-  }
-  use {
-    'nvim-tree/nvim-tree.lua',
-    requires = {
-      {'nvim-tree/nvim-web-devicons'},
-    },
-    config = require("nvim-tree").setup({
-      view = {
-        adaptive_size = true
-      },
-      update_focused_file = {
-        enable = true,
-        update_cwd = true,
-      },
-    })
-  }
-  use {
-    'nvim-lualine/lualine.nvim',
-    config = require'lualine'.setup{
-      options = {
-        theme = 'tokyonight',
-        icons_enabled = true,
-      },
-      sections = {
-        lualine_a = {
-          {
-            'filename',
-            path = 1,
-          }
-        },
-        lualine_c = {
-          'data',
-          "require'lsp-status'.status()" }
-        }
-      },
-      requires = {
-        {'nvim-lua/lsp-status.nvim'},
-      }
-    }
+	use 'vim-crystal/vim-crystal'
 
-  use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.1',
-    requires = { {'nvim-lua/plenary.nvim'} }
-  }
+	use {
+		'folke/tokyonight.nvim',
+		config = function() require('tokyonight').setup({
+			style = 'storm'
+		}) end
+	}
+	use {
+		'nvim-tree/nvim-tree.lua',
+		requires = {
+			{'nvim-tree/nvim-web-devicons'},
+		},
+		config = function() require("nvim-tree").setup({
+			view = {
+				adaptive_size = true
+			},
+			update_focused_file = {
+				enable = true,
+				update_cwd = true,
+			},
+		}) end
+	}
+	use {
+		'nvim-lualine/lualine.nvim',
+		config = function() require'lualine'.setup{
+			options = {
+				theme = 'tokyonight',
+				icons_enabled = true,
+			},
+			sections = {
+				lualine_a = {
+					{
+						'filename',
+						path = 1,
+					}
+				},
+				lualine_c = {
+					'data',
+					"require'lsp-status'.status()" }
+				}
+			} end,
+			requires = {
+				{'nvim-lua/lsp-status.nvim'},
+			}
+		}
 
-  use {
-    'VonHeikemen/lsp-zero.nvim',
-    requires = {
-      -- LSP Support
-      {'neovim/nvim-lspconfig'},
-      {'williamboman/mason.nvim'},
-      {'williamboman/mason-lspconfig.nvim'},
+		use {
+			'nvim-telescope/telescope.nvim', tag = '0.1.1',
+			requires = { {'nvim-lua/plenary.nvim'} }
+		}
 
-      -- Autocompletion
-      {'hrsh7th/nvim-cmp'},
-      {'hrsh7th/cmp-buffer'},
-      {'hrsh7th/cmp-path'},
-      {'saadparwaiz1/cmp_luasnip'},
-      {'hrsh7th/cmp-nvim-lsp'},
-      {'hrsh7th/cmp-nvim-lua'},
+		use {
+			'VonHeikemen/lsp-zero.nvim',
+			requires = {
+				-- LSP Support
+				{'neovim/nvim-lspconfig'},
+				{'williamboman/mason.nvim'},
+				{'williamboman/mason-lspconfig.nvim'},
 
-      -- Snippets
-      {'L3MON4D3/LuaSnip'},
-      {'rafamadriz/friendly-snippets'},
+				-- Autocompletion
+				{'hrsh7th/nvim-cmp'},
+				{'hrsh7th/cmp-buffer'},
+				{'hrsh7th/cmp-path'},
+				{'saadparwaiz1/cmp_luasnip'},
+				{'hrsh7th/cmp-nvim-lsp'},
+				{'hrsh7th/cmp-nvim-lua'},
 
-      -- neovim lua config support
-      {
-        'folke/neodev.nvim',
-        config = require("neodev").setup({})
-      },
-  }
-}
+				-- Snippets
+				{'L3MON4D3/LuaSnip'},
+				{'rafamadriz/friendly-snippets'},
 
-use 'neovim/nvim-lspconfig'
+				-- neovim lua config support
+				{
+					'folke/neodev.nvim',
+					config = function () require("neodev").setup({}) end
+				},
+			}
+		}
 
-use {
-  'nvim-treesitter/nvim-treesitter',
-  run = function()
-    local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-    ts_update()
-  end,
-  config = require'nvim-treesitter.configs'.setup {
-    ensure_installed = { "vimdoc", "javascript", "typescript", "c", "lua", "rust" },
-    sync_install = false,
-    auto_install = true,
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = false,
-    },
-  }
-}
-  end)
+		use 'neovim/nvim-lspconfig'
+
+		use {
+			'nvim-treesitter/nvim-treesitter',
+			run = function()
+				local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+				ts_update()
+			end,
+			config = function () require'nvim-treesitter.configs'.setup {
+				ensure_installed = { "vimdoc", "javascript", "typescript", "c", "lua", "rust" },
+				sync_install = false,
+				auto_install = true,
+				highlight = {
+					enable = true,
+					additional_vim_regex_highlighting = false,
+				},
+			} end
+		}
+
+		use {'nvim-treesitter/nvim-treesitter-context',
+		config = function () require'treesitter-context'.setup({enable = true}) end
+	}
+
+end)
